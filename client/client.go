fecd47db91fd0c5970df66d132b11283497b65c5
kjense54 <iamkatelynj@gmail.com>
2024-04-27 14:45:14 -0600

    send startup message from client to server

--- client/client.go
+++ client/client.go
@@ -1,23 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"net"
+	"context"
 	"log"
-	//"context"
+	"net"
+	"time"	
+	"fmt"
 )
 
 func main() {
-	fmt.Println("Client Started")
+	var d net.Dialer
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
 
-	localAddr := "localhost:8090"
-	conn, err := net.Dial("tcp", localAddr)
+	conn, err := d.DialContext(ctx, "tcp", "localhost:8090")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to dial: %v", err)
 	}
 	defer conn.Close()
+	
+	// client loop
+	for {
+		// input channel
+		inputCh := make(chan string)
+		go func() {
+			var client_input string
+			for {
+				fmt.Scanln(&client_input)
+				inputCh <- client_input
+			}
+		}()
+	
+		// write test message
+		go func() {
+			message := ("Hello from client!")
+			
+			if _, err := conn.Write([]byte(string(len(message)) + message)); err != nil {
+				log.Fatal(err)
+			}
+		}()
 
-	if _, err := conn.Write([]byte("Hello!")); err != nil {
-		log.Fatal(err)
+		// quit client
+		select {
+		case input := <-inputCh:
+			if input == "/quit" {
+				fmt.Println("Quitting")
+				return
+			}
+		}
 	}
 }
--- server/server.go
+++ server/server.go
@@ -5,19 +5,33 @@ import (
 	"io"
 	"log"
 	"fmt"
+	"bufio"
 )
 
-func handleConnection(c net.Conn) {
-	io.Copy(c, c) // echo incoming data
-}
+func handleConnection(conn net.Conn) {
+	defer func() {
+		fmt.Println("Client at ", conn.RemoteAddr().String(), " disconnected")
+		conn.Close()
+	}()
 
-func handleListen(l net.Listener) {
-	conn, err := l.Accept()
-	if err != nil {
-		log.Fatal(err)
+	buff := make([]byte, 50) // largest msg
+	c := bufio.NewReader(conn)
+	
+	// receive loop
+	for {
+		//read first byte for message length
+		size, err := c.ReadByte()
+		if err != nil {
+			fmt.Println("Client disconnected")
+			return	
+		}
+		// read full message
+		_, err1 := io.ReadFull(c, buff[:int(size)])
+		if err1 != nil {
+			return
+		}
+		fmt.Println(string(buff[:int(size)]))
 	}
-	fmt.Print("Client connected!")
-	go handleConnection(conn)
 }
 
 func main() {
@@ -31,23 +45,33 @@ func main() {
 	// close listener after main ends
 	defer l.Close()
 
-	// receive server terminal input to channel
-	inputCh := make(chan string) 
-	go func() {
-		var server_input string 
-		for {
-			fmt.Scanln(&server_input)
-			inputCh <- server_input
-		}
-	}()
 
 	for {
-		go handleListen(l)
+		fmt.Println("Server Started")
+		// input channel
+		inputCh := make(chan string)
+		go func() {
+			var server_input string 
+			for {
+				fmt.Scanln(&server_input)
+				inputCh <- server_input
+			}
+		}()
+		
+		// wait for connection
+		go func() {
+			conn, err := l.Accept()
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println("Client ", conn.RemoteAddr().String(), " Connected!")
+			go handleConnection(conn)
+		}()
 
 		//quit server
 		select {
 		case input := <-inputCh:
-			if input == "quit" {
+			if input == "/quit" {
 				fmt.Println("Quitting")
 				return
 			}
